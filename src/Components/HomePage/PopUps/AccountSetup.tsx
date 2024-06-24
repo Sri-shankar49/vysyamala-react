441ca47b6c94e77bc8bc065524d0d91e86670f56
Sri-shankar49 <srishankar4499@gmail.com>
2024-06-24 10:52:13 +0530

    commit feat : react hook form pkg & accountSetup popup

--- src/Components/HomePage/PopUps/AccountSetup.tsx
+++ src/Components/HomePage/PopUps/AccountSetup.tsx
@@ -1,19 +1,32 @@
 import React, { useState } from 'react';
 import { IoEye, IoEyeOff } from "react-icons/io5";
 import { IoIosCloseCircle } from "react-icons/io";
+import { useForm, SubmitHandler } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as zod from "zod"
 
+// ZOD Schema
+const schema = zod.object({
+    profileFor: zod.string().min(1, 'Profile for is required'),
+    mobile: zod.string().min(10, 'Mobile number must be exactly 10 characters').max(10, 'Mobile number must be exactly 10 characters'),
+    email: zod.string().email('Invalid email address'),
+    password: zod.string().min(6, 'Password must be at least 6 characters'),
+}).required();
 
 interface AccountSetupProps {
     onNext: () => void;
     onClose: () => void;
 }
 
-export const AccountSetup: React.FC<AccountSetupProps> = ({ onNext, onClose }) => {
+// React Hook Form input type props
+interface FormInputs {
+    profileFor: string;
+    mobile: string;
+    email: string;
+    password: string;
+}
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        onNext();
-    };
+export const AccountSetup: React.FC<AccountSetupProps> = ({ onNext, onClose }) => {
 
     // Toggle the Password field
     const [showPassword, setShowPassword] = useState(false);
@@ -22,10 +35,17 @@ export const AccountSetup: React.FC<AccountSetupProps> = ({ onNext, onClose }) =
         setShowPassword(!showPassword);
     };
 
-    // console.log('AccountSetup component rendered'); // Debug log
+    // React Hook form
+    const { register, handleSubmit, formState: { errors } } = useForm<FormInputs>({ resolver: zodResolver(schema), });
+
+    const onSubmit: SubmitHandler<FormInputs> = (data) => {
+        console.log(data);
+        onNext();
+    };
+
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <div>
                 <p className="text-[16px] text-primary">While we find Matches for you</p>
                 <h5 className="text-[24px] text-primary font-semibold mb-5">Let's set up your account</h5>
@@ -38,13 +58,15 @@ export const AccountSetup: React.FC<AccountSetupProps> = ({ onNext, onClose }) =
                 <select
                     id="profileFor"
                     className="text-ash font-medium block w-full px-3 py-2 border-[1px] border-footer-text-gray rounded-[4px] focus-visible:outline-none"
+                    {...register("profileFor")}
                 >
-                    <option value="Matrimony Profile for" selected disabled>Matrimony Profile for</option>
-                    <option value="Your self">Your self</option>
-                    <option value="son / Daughter">Son / Daughter</option>
+                    <option value="Select your Matrimony Profile for">Select your Matrimony Profile for</option>
+                    <option value="Myself">Myself</option>
+                    <option value="Son / Daughter">Son / Daughter</option>
                     <option value="Relatives">Relatives</option>
                     <option value="Friends">Friends</option>
                 </select>
+                {errors.profileFor && <span className="text-red-500">{errors.profileFor.message}</span>}
             </div>
 
             <div className="mb-5">
@@ -52,11 +74,13 @@ export const AccountSetup: React.FC<AccountSetupProps> = ({ onNext, onClose }) =
                     Mobile number
                 </label> */}
                 <input
-                    type="tel"
+                    type="number"
                     id="mobile"
                     className="w-full px-3 py-2 text-ash border-[1px] border-footer-text-gray rounded-[4px] focus-visible:outline-none"
                     placeholder="Mobile Number"
+                    {...register("mobile")}
                 />
+                {errors.mobile && <span className="text-red-500">{errors.mobile.message}</span>}
             </div>
 
             <div className="mb-5">
@@ -68,7 +92,9 @@ export const AccountSetup: React.FC<AccountSetupProps> = ({ onNext, onClose }) =
                     id="email"
                     className="w-full px-3 py-2 text-ash border-[1px] border-footer-text-gray rounded-[4px] focus-visible:outline-none"
                     placeholder="Email"
+                    {...register("email")}
                 />
+                {errors.email && <span className="text-red-500">{errors.email.message}</span>}
             </div>
 
             <div className="mb-5">
@@ -81,12 +107,14 @@ export const AccountSetup: React.FC<AccountSetupProps> = ({ onNext, onClose }) =
                         id="password"
                         className="w-full px-3 py-2 text-ash border-[1px] border-footer-text-gray rounded-[4px] focus-visible:outline-none"
                         placeholder="Create Password"
+                        {...register("password")}
                     />
 
                     <div onClick={handleShowPassword} className="absolute inset-y-1.5 right-0 pr-3 flex items-center text-ash text-[18px] cursor-pointer">
                         {showPassword ? <IoEyeOff /> : <IoEye />}
                     </div>
                 </div>
+                {errors.password && <span className="text-red-500">{errors.password.message}</span>}
             </div>
 
             <button
@@ -106,7 +134,7 @@ export const AccountSetup: React.FC<AccountSetupProps> = ({ onNext, onClose }) =
                     Login
                 </button>
             </p>
-            
+
             {/* <button
                 type="button"
                 onClick={onClose}
@@ -116,7 +144,7 @@ export const AccountSetup: React.FC<AccountSetupProps> = ({ onNext, onClose }) =
             </button> */}
 
             <IoIosCloseCircle onClick={onClose} className="absolute top-[-15px] right-[-15px] text-[30px] text-black bg-white rounded-full flex items-center cursor-pointer hover:text-white hover:bg-black" />
-        </form>
+        </form >
     );
 };
 
